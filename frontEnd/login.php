b978e3341f3ee734c3c07121a7da7bd0dbb1f119
César Willian Pacheco <cwillianpacheco@gmail.com>
2023-04-17 11:42:51 -0300

    Update login.php

--- frontEnd/login.php
+++ frontEnd/login.php
@@ -1,19 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Teto Facil</title>
 </head>
+
 <body>
     <p>Página de Login</p>
     <form action="" method="post">
-        Login: <input type="text" placeholder = "Digite seu login: ">
+        Login: <input type="text" placeholder="Digite seu login: ">
         <br>
-        Senha: <input type="text" placeholder = "Digite sua senha: "> 
+        Senha: <input type="text" placeholder="Digite sua senha: ">
         <br>
         <input type="submit" value="Entrar" id="submitLogin" name="SubmitLogin">
     </form>
 </body>
+
+<?php
+    echo "Teste";
+?>
+
 </html>
