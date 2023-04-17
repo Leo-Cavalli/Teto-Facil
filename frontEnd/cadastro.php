6a21693cd77d0e445fa8e3d7700ff331c6244452
OtvCng <otavionogueira@hotmail.com>
2023-04-17 11:06:23 -0300

    login/cadastro cru

---
+++ frontEnd/cadastro.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Teto facil</title>
+</head>
+<body>
+  <h1>Pagina de cadastro</h1>
+  <form action="" method="post">
+    Nome: <input type="text" placeholder ="Digite seu nome:">
+    <br>
+    Email: <input type="email" name="emailCadastro" id="email" placeholder="Digite seu email:">
+    <br>
+    Senha: <input type="password" name="senhaCadastro" id="senha" placeholder="Digite sua senha:">
+    <br>
+    <input type="submit" value="Criar conta" id="submitCriar">
+  </form>
+</body>
+</html>
