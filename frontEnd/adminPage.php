67f181710a6bf7eb5ae57901c999695e83f3b39e
Eduardo Mussi <eduardomazzarotto@outlook.com>
2023-05-11 17:20:45 -0300

    Docs Folder Creation

--- frontEnd/adminPage.php
+++ frontEnd/adminPage.php
@@ -5,12 +5,14 @@
         $msgCad = $_GET['msgCad'];
     }
  ?>
-<!DOCTYPE html>
+<!DOCTYPE html5>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Stylesheets/normalize.css">
+    <link rel="stylesheet" href="Stylesheets/admin.css">
     <title>Página de Administrador</title>
 </head>
 <body>
