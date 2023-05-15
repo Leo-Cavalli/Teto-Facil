d7ca7b797b435e9a971b806ec75ec139cef82224
RodrigoMunch <munchrodrigo@gmail.com>
2023-05-15 08:20:06 -0300

    css edit agent

--- frontEnd/adminPage.php
+++ frontEnd/adminPage.php
@@ -45,6 +45,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Stylesheets/admin_mint.css">
+
     <title>Página de Administrador</title>
 </head>
 <body>
