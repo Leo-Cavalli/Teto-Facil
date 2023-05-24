dd1f1069b1e9250e22c9a4dc1305a8f795575427
Leo-Cavalli <leocavalli0203@gmail.com>
2023-05-24 10:05:13 -0300

    Bug Fix

--- frontEnd/adminPage.php
+++ frontEnd/adminPage.php
@@ -52,7 +52,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="Stylesheets/normalize.css">
-    <link rel="stylesheet" href="Stylesheets/admin.css">
+    <link rel="stylesheet" href="Stylesheets/admin_mint.css">
     <title>Página de Administrador</title>
 </head>
 <body>
